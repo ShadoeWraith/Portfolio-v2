4df8feb6c928152f51e27ebd32c103dfa5800bc2
Shaun Kiszonas <shadoewraith99@gmail.com>
2022-10-03 12:11:16 -0600

    added project

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,32 +2,25 @@ import AirPollution from '../../assets/AirPollution.jpg';
 import ParkPlace from '../../assets/ParkPlace.jpg';
 import CounselorReservation from '../../assets/CounselorReservation.jpg';
 import tranquility from '../../assets/tranquility.jpg';
-import github from '../../assets/github.jpg';
+import landingpage from '../../assets/Landing-Page.jpg';
 
 import './Card.css';
 
 export default function Card() {
   const cardData = [
     {
-      title: 'Air Quality',
-      image: AirPollution,
-      github: 'https://github.com/Callank21/group-project',
-      deployed: 'https://callank21.github.io/group-project/index.html',
-      technology: ['HTML,', ' JavaScript,', ' TailwindCSS,', ' REST API'],
+      title: 'FFXIV Tranquility',
+      image: tranquility,
+      github: 'https://github.com/ShadoeWraith/tranquility',
+      deployed: 'https://shadoewraith.github.io/tranquility/',
+      technology: ['HTML,', ' CSS,', ' JavaScript,', ' React.js,', ' REST API'],
     },
     {
-      title: 'Park Place',
-      image: ParkPlace,
-      github:
-        'https://github.com/SalvadorBanuelos424/Park-Place-master-planned-community',
-      deployed: 'https://park-place-association.herokuapp.com/',
-      technology: [
-        'Handlebars,',
-        ' JavaScript,',
-        ' TailwindCSS,',
-        ' MySQL,',
-        ' Express.js',
-      ],
+      title: 'Landing Page',
+      image: landingpage,
+      github: 'https://github.com/ShadoeWraith/landing-page',
+      deployed: 'https://shadoewraith.github.io/landing-page/',
+      technology: ['HTML,', ' CSS,', ' Bootstrap'],
     },
     {
       title: 'Counselor Reservation',
@@ -44,11 +37,25 @@ export default function Card() {
       ],
     },
     {
-      title: 'FFXIV Tranquility',
-      image: tranquility,
-      github: 'https://github.com/ShadoeWraith/tranquility',
-      deployed: 'https://shadoewraith.github.io/tranquility/',
-      technology: ['HTML,', ' CSS,', ' JavaScript,', ' React.js,', ' REST API'],
+      title: 'Park Place',
+      image: ParkPlace,
+      github:
+        'https://github.com/SalvadorBanuelos424/Park-Place-master-planned-community',
+      deployed: 'https://park-place-association.herokuapp.com/',
+      technology: [
+        'Handlebars,',
+        ' JavaScript,',
+        ' TailwindCSS,',
+        ' MySQL,',
+        ' Express.js',
+      ],
+    },
+    {
+      title: 'Air Quality',
+      image: AirPollution,
+      github: 'https://github.com/Callank21/group-project',
+      deployed: 'https://callank21.github.io/group-project/index.html',
+      technology: ['HTML,', ' JavaScript,', ' TailwindCSS,', ' REST API'],
     },
   ];
   return (
