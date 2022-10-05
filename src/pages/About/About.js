c0682aab3a71066550da6f4b4b583cee1345e30f
Shaun Kiszonas <shadoewraith99@gmail.com>
2022-10-05 14:02:46 -0600

    Adjusted some wording

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -10,7 +10,7 @@ export default function About() {
       <div className='about-content'>
         <div>
           <p>
-            Front end web developer learning new technologies to better improve
+            Full stack web developer learning new technologies to better improve
             my ability to create well maintainable web apps. I think creatively
             and logically when creating web apps to best suit the overall user
             experience.
@@ -18,7 +18,7 @@ export default function About() {
           <h4>Skills:</h4>
           <h5>
             HTML, CSS, JavaScript, JQuery, React.js, Bootstrap, TailWindCSS,
-            Node.js
+            Node.js, MySQL
           </h5>
         </div>
         <img className='about-image' src={selfie} alt='selfie' />
