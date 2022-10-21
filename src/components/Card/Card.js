7117eb910f56633e3df7322adb75bde312af17a9
Shaun Kiszonas <shadoewraith99@gmail.com>
2022-10-21 14:28:12 -0600

    added an expand projects button

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,8 +4,11 @@ import tranquility from '../../assets/tranquility.jpg';
 import landingpage from '../../assets/Landing-Page.jpg';
 import portfolioV1 from '../../assets/Portfolio_v1.jpg';
 import weatherApp from '../../assets/WeatherDashboard.JPG';
+import AirPollution from '../../assets/AirPollution.jpg';
+import ParkPlace from '../../assets/ParkPlace.jpg';
 
 import './Card.css';
+import { useState } from 'react';
 
 export default function Card() {
   const cardData = [
@@ -60,27 +63,92 @@ export default function Card() {
       ],
     },
   ];
+
+  const secondaryCardData = [
+    {
+      title: 'Park Place',
+      image: ParkPlace,
+      github:
+        'https://github.com/SalvadorBanuelos424/Park-Place-master-planned-community',
+      deployed: 'https://park-place-association.herokuapp.com/',
+      technology: [
+        'HTML,',
+        ' TailWindCSS,',
+        ' JavaScript,',
+        ' Handlebars,',
+        ' MySQL',
+      ],
+    },
+    {
+      title: 'Air Pollution',
+      image: AirPollution,
+      github: 'https://github.com/Callank21/group-project',
+      deployed: 'https://callank21.github.io/group-project/index.html',
+      technology: ['HTML,', ' TailWindCSS,', ' JavaScript, ', ' REST API'],
+    },
+  ];
+
+  const [viewMore, setViewMore] = useState(false);
+
   return (
-    <div className='card-wrapper'>
-      {cardData.map((data) => (
-        <div className='card' href={data.deployed} key={data.title}>
-          <div className='card-image'>
-            <img className='image' src={data.image} alt={data.image} />
-          </div>
-          <div className='card-footer'>
-            <span className='card-tech'>{data.technology}</span>
-            <h4>{data.title}</h4>
-            <div className='card-links'>
-              <a className='card-deploy' href={data.deployed}>
-                Show Project{' '}
-              </a>
-              <a className='card-github' href={data.github}>
-                GitHub
-              </a>
+    <div>
+      <div className='card-wrapper'>
+        {cardData.map((data) => (
+          <div className='card' href={data.deployed} key={data.title}>
+            <div className='card-image'>
+              <img className='image' src={data.image} alt={data.image} />
+            </div>
+            <div className='card-footer'>
+              <span className='card-tech'>{data.technology}</span>
+              <h4>{data.title}</h4>
+              <div className='card-links'>
+                <a className='card-deploy' href={data.deployed}>
+                  Show Project{' '}
+                </a>
+                <a className='card-github' href={data.github}>
+                  GitHub
+                </a>
+              </div>
             </div>
           </div>
+        ))}
+      </div>
+      {!viewMore ? (
+        <div className='button-wrapper'>
+          <button className='viewBtn' onClick={() => setViewMore(true)}>
+            Expand Projects
+          </button>
         </div>
-      ))}
+      ) : (
+        <div>
+          <div className='card-wrapper'>
+            {secondaryCardData.map((data) => (
+              <div className='card' href={data.deployed} key={data.title}>
+                <div className='card-image'>
+                  <img className='image' src={data.image} alt={data.image} />
+                </div>
+                <div className='card-footer'>
+                  <span className='card-tech'>{data.technology}</span>
+                  <h4>{data.title}</h4>
+                  <div className='card-links'>
+                    <a className='card-deploy' href={data.deployed}>
+                      Show Project{' '}
+                    </a>
+                    <a className='card-github' href={data.github}>
+                      GitHub
+                    </a>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className='button-wrapper'>
+            <button className='viewBtn' onClick={() => setViewMore(false)}>
+              Collapse Projects
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
